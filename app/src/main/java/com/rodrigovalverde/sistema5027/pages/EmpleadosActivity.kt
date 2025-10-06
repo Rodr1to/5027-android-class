a6272bac88f82d56b8fb879b5e1c9bb0791a8da4
rodr1to <rovalverdea@gmail.com>
2025-10-06 01:52:16 -0500

    update: EmpleadosActivity.kt, updated from linearprogressindicator to circularprogressindicator, took out scaffold, modifications on fun Empleado

--- app/src/main/java/com/rodrigovalverde/sistema5027/pages/EmpleadosActivity.kt
+++ app/src/main/java/com/rodrigovalverde/sistema5027/pages/EmpleadosActivity.kt
@@ -1,17 +1,19 @@
 package com.rodrigovalverde.sistema5027.pages
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.LinearProgressIndicator
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -19,8 +21,15 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
 import com.rodrigovalverde.sistema5027.models.Empleado
 import com.rodrigovalverde.sistema5027.ui.theme.Sistema5027Theme
@@ -28,6 +37,7 @@ import com.rodrigovalverde.sistema5027.utils.API_URL
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
+import com.rodrigovalverde.sistema5027.R
 
 interface EmpleadosService {
     @GET("empleados")
@@ -61,39 +71,65 @@ class EmpleadosActivity : ComponentActivity() {
                     isLoading = false
                 }
 
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column(modifier = Modifier.padding(innerPadding)) {
+                    Column(modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center) {
                         if(isLoading){
-                            LinearProgressIndicator()
+                            // LinearProgressIndicator()
+                            CircularProgressIndicator()
                         } else {
                             LazyRow {
                                 items(items = listaEmpleados!!){ itemEmpleado ->
 
-                                    Box {
-                                        AsyncImage(
-                                            API_URL + itemEmpleado.foto, null,
-                                            modifier = Modifier.fillParentMaxSize(),
-                                            contentScale = ContentScale.Crop
-                                        )
-                                        DibujarEmpleado(itemEmpleado)
-                                    }
+
+                                        DibujarEmpleado(itemEmpleado, modifier = Modifier.fillParentMaxSize())
+
                                 }
                             }
 
                         }
                     }
-                }
             }
         }
     }
 }
 
+@SuppressLint("ConfigurationScreenWidthHeight")
 @Composable
 
-fun DibujarEmpleado(itemEmpleado: Empleado) {
-        Column {
-            Text(text = itemEmpleado.nombres)
-            Text(text = itemEmpleado.apellidos)
-            Text(text = itemEmpleado.cargo)
+fun DibujarEmpleado(itemEmpleado: Empleado, modifier: Modifier) {
+    val configuration = LocalConfiguration.current
+    val screenHeightPx = with(LocalDensity.current){
+        configuration.screenHeightDp.dp.toPx()
+    }
+
+    Box (modifier = modifier) {
+        AsyncImage(
+            API_URL + itemEmpleado.foto, null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
+
+        Column(modifier = Modifier.fillMaxSize()
+            .background(Brush.verticalGradient(
+                colors = listOf(Color.Transparent, Color.Black),
+                startY = screenHeightPx * 0.75f,
+                endY = screenHeightPx * 1f,
+
+            ))
+        ){ }
+
+        Column (modifier = Modifier.fillMaxSize().padding(dimensionResource(R.dimen.espacio_2)),
+            horizontalAlignment = Alignment.End,
+            verticalArrangement = Arrangement.Bottom){
+            Text(text = itemEmpleado.nombres + " " + itemEmpleado.apellidos,
+                modifier = Modifier.background(Color.White)
+                    .padding(horizontal = dimensionResource(R.dimen.espacio_2)))
+            Text(text = itemEmpleado.cargo,
+                modifier = Modifier.background(Color.Black)
+                    .padding(horizontal = dimensionResource(R.dimen.espacio_2)),
+                color = Color.White)
+
         }
+    }
 }
